1a550afb604cb35d547cbc9c17ade6cfd3b79f38
Johannes Klein <johannes.t.klein@gmail.com>
2022-01-26 12:39:21 +0100

    Display network component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Grid from "@material-ui/core/Grid";
 
-// import NetworkComponent from "./components/NetworkComponent";
+import NetworkComponent from "./components/NetworkComponent";
 // import Explanation from "./Explanation";
 
 import "./App.css";
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Grid container>
-          {/* <NetworkComponent /> */}
         <Grid item className="grid-item section section-0" xs={12}>
+          <NetworkComponent />
         </Grid>
         <Grid item className="grid-item section section-1" xs={12}>
           {/* <Explanation /> */}
