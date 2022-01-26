b5cc8864ce32290afa5c62335c7e094a22df2177
Johannes Klein <johannes.t.klein@gmail.com>
2022-01-26 12:38:08 +0100

    Rename css classes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="App">
       <Grid container>
-        <Grid item className="grid-item section section-1" xs={12}>
           {/* <NetworkComponent /> */}
+        <Grid item className="grid-item section section-0" xs={12}>
         </Grid>
-        <Grid item className="grid-item section section-2" xs={12}>
+        <Grid item className="grid-item section section-1" xs={12}>
           {/* <Explanation /> */}
         </Grid>
       </Grid>
