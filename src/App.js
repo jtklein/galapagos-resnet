b217c0c8fd9a30623acdfcc9211dc013ca8f314d
Johannes Klein <johannes.t.klein@gmail.com>
2022-01-27 16:07:11 +0100

    Remove unused grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import Grid from "@material-ui/core/Grid";
-
 import DataComponent from "./components/DataComponent";
-// import Explanation from "./Explanation";
 
 import "./App.css";
 
@@ -9,14 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Grid container>
-        <Grid item className="grid-item section section-0" xs={12}>
-          <DataComponent />
-        </Grid>
-        <Grid item className="grid-item section section-1" xs={12}>
-          {/* <Explanation /> */}
-        </Grid>
-      </Grid>
+      <DataComponent />
     </div>
   );
 }
