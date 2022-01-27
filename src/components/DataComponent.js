ff20574fbad66ada99f70ccb862b177af3efe76f
Johannes Klein <johannes.t.klein@gmail.com>
2022-01-27 16:04:50 +0100

    Add a map component to the website

--- src/components/DataComponent.js
+++ src/components/DataComponent.js
@@ -1,11 +1,101 @@
 import React, { Component } from "react";
+import _ from "lodash";
 import Grid from "@material-ui/core/Grid";
 
 import NetworkContainer from "./NetworkContainer2";
-// import Explanation from "./Explanation";
+import MapComponent from "./MapComponent";
 import { data } from "./RI";
 
 const initialData = Object.assign({}, data);
+
+const testCoordinates = {
+  0: {
+    title:
+      "Estudio del estado de las poblacion de pinguino, cormoran no volador y albatros de Galápagos",
+    "Lead researcher": "Gustavo Jimenez",
+    locations: [
+      {
+        Island: "Fernandina",
+        Site: "Punta Espinoza",
+        lat: -0.262757801,
+        lng: -91.44395571,
+      },
+      {
+        Island: "Isabela",
+        Site: "Marielas",
+        lat: -0.594764945,
+        lng: -91.09091762,
+      },
+      {
+        Island: "Isabela",
+        Site: "Punta Albemarle",
+        lat: 0.163234486,
+        lng: -91.34252459,
+      },
+      {
+        Island: "Española",
+        Site: "Punta Suarez",
+        lat: -1.368599847,
+        lng: -89.7455209,
+      },
+      {
+        Island: "Isabela",
+        Site: "Puerto Pajas",
+        lat: -0.755,
+        lng: -91.375,
+      },
+    ],
+  },
+  1: {
+    title:
+      "Estudio del estado de la poblacion del flamenco de Galapagos y aves de laguna.",
+    "Lead researcher": "Gustavo Jimenez",
+    locations: [
+      {
+        Island: "Floreana",
+        Site: "Montura",
+        lat: -1.315595325,
+        lng: -90.51053316,
+      },
+      {
+        Island: "Floreana",
+        Site: "Punta Cormorant",
+        lat: -1.221551065,
+        lng: -90.42814406,
+      },
+      {
+        Island: "Santiago",
+        Site: "Sartén",
+        lat: -0.35643209,
+        lng: -90.66205688,
+      },
+      {
+        Island: "Santa Cruz",
+        Site: "Garrapatero",
+        lat: -0.692977287,
+        lng: -90.2220813,
+      },
+      {
+        Island: "Santa Cruz",
+        Site: "Tortuga",
+        lat: -0.761056229,
+        lng: -90.34315443,
+      },
+      {
+        Island: "Isabela",
+        Site: "Barahona 1",
+        lat: -0.996809427,
+        lng: -91.05485548,
+      },
+      {
+        Island: "Santiago",
+        Site: "Bainbridge",
+        lat: -0.351995867,
+        lng: -90.56585229,
+      },
+    ],
+  },
+};
 
 class DataComponent extends Component {
   constructor(props) {
@@ -40,7 +130,10 @@ class DataComponent extends Component {
         });
       // Map over all links again to get all ids of second degree connected nodes
       data.links
-        .filter((link) => connectedIDs.has(link.source.id) || connectedIDs.has(link.target.id))
+        .filter(
+          (link) =>
+            connectedIDs.has(link.source.id) || connectedIDs.has(link.target.id)
+        )
         .map((link) => {
           connectedIDs.add(link.source.id);
           connectedIDs.add(link.target.id);
@@ -73,8 +166,22 @@ class DataComponent extends Component {
     }
   };
 
+  onMarkerClicked = (marker, callback) => {
+    const { selectedNode } = this.state;
+    console.log('marker :>> ', marker);
+  };
+
   render() {
     const { shownData, selectedNode } = this.state;
+    let markers = [];
+    _.map(testCoordinates, (value) => {
+      markers = markers.concat(value.locations);
+    });
+    if (selectedNode) {
+      markers = testCoordinates.hasOwnProperty(selectedNode.id)
+        ? testCoordinates[selectedNode.id].locations
+        : [];
+    }
     return (
       <Grid container>
         <Grid item className="grid-item section section-0" xs={6}>
@@ -85,8 +192,10 @@ class DataComponent extends Component {
           />
         </Grid>
         <Grid item className="grid-item section section-1" xs={6}>
-          <div>Map goes here</div>
-          {/* <Explanation /> */}
+          <MapComponent
+            markers={markers}
+            onMarkerClicked={(m, cb) => this.onMarkerClicked(m, cb)}
+          />
         </Grid>
       </Grid>
     );
