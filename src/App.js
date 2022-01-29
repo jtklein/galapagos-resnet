e9133a9708a8bd2b22fd9d0bad57b8070e8711ce
Johannes Klein <johannes.t.klein@gmail.com>
2022-01-29 11:45:48 +0100

    Disclaimer that it does not look good on mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import Typography from "@material-ui/core/Typography";
+import { isMobile } from "react-device-detect";
+
 import DataComponent from "./components/DataComponent";
 
 import "./App.css";
 
-
 function App() {
   return (
     <div className="App">
-      <DataComponent />
+      {isMobile ? (
+        <Typography
+          variant="h1"
+          style={{
+            borderTop: "1px solid #ccc",
+            marginTop: -10,
+            paddingTop: 10,
+          }}
+        >
+          {"This website does not look good on mobile phones. Try it on a laptop or desktop computer instead."}
+        </Typography>
+      ) : (
+        <DataComponent />
+      )}
     </div>
   );
 }
