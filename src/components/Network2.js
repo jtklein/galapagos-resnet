8c37f8fa90fe3e9284c3b6c951885a9b79d6f7ff
Johannes Klein <johannes.t.klein@gmail.com>
2022-02-12 11:12:48 +0100

    Change link opacity

--- src/components/Network2.js
+++ src/components/Network2.js
@@ -60,6 +60,7 @@ export default function Network(el, props) {
     .data(linksOfSelectedNode)
     .join("line")
     .classed("link", true)
+    .attr("opacity", 0.6)
     .attr("stroke", function (d) {
       return d.color;
     })
