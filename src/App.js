5586ca260662241d5a8a2ca0b4caf71522347052
Johannes Klein <johannes.t.klein@gmail.com>
2022-05-05 10:33:14 +0200

    Remove on mobile text placeholder

--- src/App.js
+++ src/App.js
@@ -14,37 +14,21 @@ function App() {
   return (
     <div className="App">
       <TitlePanel />
-
-      {isMobile ? (
-        <Typography
-          variant="h1"
-          style={{
-            borderTop: "1px solid #ccc",
-            marginTop: -10,
-            paddingTop: 10,
-          }}
-        >
-          {
-            "This website does not look good on mobile phones. Try it on a laptop or desktop computer instead."
-          }
-        </Typography>
-      ) : (
-        <Grid container>
-          {isMobile ? (
-            <Grid item className="grid-item section section-0" xs={12}>
-              <Typography variant="h5" gutterBottom style={{ color: "#666" }}>
-                {t("subtitle")}
-              </Typography>
-            </Grid>
-          ) : null}
-          <Grid item className="grid-item section section-1" xs={12}>
-            <DataComponent />
+      <Grid container>
+        {isMobile ? (
+          <Grid item className="grid-item section section-0" xs={12}>
+            <Typography variant="h5" gutterBottom style={{ color: "#666" }}>
+              {t("subtitle")}
+            </Typography>
           </Grid>
-          <Grid item className="grid-item section section-2" xs={12}>
-            <Explanation />
-          </Grid>
+        ) : null}
+        <Grid item className="grid-item section section-1" xs={12}>
+          <DataComponent />
         </Grid>
-      )}
+        <Grid item className="grid-item section section-2" xs={12}>
+          <Explanation />
+        </Grid>
+      </Grid>
     </div>
   );
 }
