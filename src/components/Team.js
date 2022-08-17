d82c2d4d788974e18bccb6d24245173c1678fa56
Johannes Klein <johannes.t.klein@gmail.com>
2022-08-17 13:50:53 +0200

    Add photos of interns

--- src/components/Team.js
+++ src/components/Team.js
@@ -8,8 +8,15 @@ import gndp from "../assets/icons/team/GNDP.png";
 import gct from "../assets/icons/team/GCT.png";
 import sophia from "../assets/icons/team/Sophia.png";
 
-const Person = ({ noLink, name, affiliation, href, src }) => (
-  <Grid item xs={4} sm={2}>
+import alejandra from "../assets/icons/team/interns/Alejandra.jpg";
+import mateo from "../assets/icons/team/interns/Mateo.jpg";
+import pablo from "../assets/icons/team/interns/Pablo.jpg";
+import rebeca from "../assets/icons/team/interns/Rebeca.jpg";
+import zuley from "../assets/icons/team/interns/Zuley.jpg";
+
+
+const Person = ({ noLink, name, affiliation, href, src, xs, sm }) => (
+  <Grid item xs={xs} sm={sm}>
     <div>
       <section>
         <p>
@@ -51,12 +58,16 @@ function Team() {
           affiliation={t("sophiaAff")}
           href={"https://www.sophiacooke.com"}
           src={sophia}
+          xs={4}
+          sm={2}
         />
         <Person
           name="Daniel Proaño"
           affiliation="FUNCAVID"
           href={"http://www.funcavid.org"}
           src={daniel}
+          xs={4}
+          sm={2}
         />
         <Person
           name={t("gnpd")}
@@ -65,18 +76,24 @@ function Team() {
             "http://areasprotegidas.ambiente.gob.ec/en/areas-protegidas/galapagos-national-park"
           }
           src={gndp}
+          xs={4}
+          sm={2}
         />
         <Person
           name={t("gct")}
           affiliation={t("gctAff")}
           href={"https://galapagosconservation.org.uk/"}
           src={gct}
+          xs={4}
+          sm={2}
         />
         <Person
           name="Dr. Johannes Klein"
           affiliation={t("johannesAff")}
           noLink
           src={johannes}
+          xs={4}
+          sm={2}
         />
       </Grid>
       <p>
@@ -91,6 +108,20 @@ function Team() {
           <a href="https://www.co-galapagos.org">info@co-galapagos.org</a>.
         </Trans>
       </p>
+      <p>{t("thanksToTeam")}</p>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="flex-end"
+        spacing={2}
+      >
+        <Person name="Alejandra Ayala" noLink src={alejandra} xs={6} sm={2} />
+        <Person name="Mateo Zaldumbide" noLink src={mateo} xs={6} sm={2} />
+        <Person name="Pablo Llerena" noLink src={pablo} xs={6} sm={2} />
+        <Person name="Rebeca Zúñiga" src={rebeca} noLink xs={6} sm={2} />
+        <Person name="Zuley Leon" noLink src={zuley} xs={6} sm={2} />
+      </Grid>
     </div>
   );
 }
